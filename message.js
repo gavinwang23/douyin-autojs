376d88ffaed9b4a368eecb7e4738919319420572
wangjunwei <863445607@qq.com>
2020-08-01 20:39:45 +0800

    message

--- message.js
+++ message.js
@@ -1,2 +1,15 @@
 console.show()
-console.log("test")
+console.log("test2")
+
+//sleep(5000)
+console.log("输入")
+id("df0").findOne().setText("hello")
+console.log("ok")
+sleep(3000)
+console.log("点击发送")
+//id("eyk").findOne().click()
+click(665,1241)
+console.log("发送成功")
+if(id("eyk").findOne().parent().exists()){
+    console.log("哈哈哈哈")
+}
